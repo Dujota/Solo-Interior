5205e73da7200efbeca80738883f3eda9ed5463b
dujota <denisdujota@gmail.com>
2022-06-28 18:22:11 -0400

    add more meta tags from html boilerplate

--- components/common/layout/Meta.jsx
+++ components/common/layout/Meta.jsx
@@ -5,6 +5,9 @@ import { META_DESCRIPTION } from 'lib/constants';
 export default function Meta() {
   return (
     <Head>
+      <meta charset="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
