4ed988ba0a9ff7308e0d968061ddf04e19a46cce
dujota <denisdujota@gmail.com>
2022-06-28 10:32:32 -0400

    add the ability to have multiple images

--- studio/schemas/documents/contact.js
+++ studio/schemas/documents/contact.js
@@ -30,7 +30,17 @@ export default {
             description:
                 "Email address",
             type: "string",
-            validation: Rule => Rule.required()
+            validation: (Rule) => {
+                Rule.regex(
+                    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+                    {
+                        name: "email", // Error message is "Does not match email-pattern"
+                        invert: false, // Boolean to allow any value that does NOT match pattern
+                    }
+                )
+
+                Rule.required()
+            }
         },
         {
             name: "slug",
