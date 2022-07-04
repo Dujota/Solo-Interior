14651b2d6cf1fd4742872e63fe4c5c81a10d4bd5
dujota <denisdujota@gmail.com>
2022-07-04 10:59:13 -0400

    add logrocket to the project free tier

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,18 @@ import '../styles/layout.css';
 import '../styles/globals.css';
 import '../styles/custom-properties.css';
 
+// you can import these packages anywhere
+import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
+
+// only initialize when in the browser
+if (typeof window !== 'undefined') {
+  console.log(LogRocket);
+  LogRocket.init('solo-interior/solo-interior');
+  // plugins should also only be initialized when in the browser
+  setupLogRocketReact(LogRocket);
+}
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
