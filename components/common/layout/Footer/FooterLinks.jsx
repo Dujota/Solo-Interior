4ad0d9e5a6fe48efd85aa087342ab18f463def4c
dujota <denisdujota@gmail.com>
2022-07-19 20:16:37 -0400

    Fixed the icon links and open in new tab

--- components/common/layout/Footer/FooterLinks.jsx
+++ components/common/layout/Footer/FooterLinks.jsx
@@ -23,7 +23,9 @@ function FooterLinks({ links = [] }) {
     <div className="flex lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
       {links.map((cta, idx) => (
         <Link href={cta.link} key={idx}>
-          <a className="text-3xl mx-3">{deriveIcon(cta.title.toLowerCase())}</a>
+          <a target="_blank" className="text-3xl mx-3">
+            {deriveIcon(cta.title.toLowerCase())}
+          </a>
         </Link>
       ))}
     </div>
@@ -33,15 +35,3 @@ function FooterLinks({ links = [] }) {
 FooterLinks.propTypes = { links: PropTypes.array };
 
 export default FooterLinks;
-
-/**
- * <a
-        href="#TODO"
-        className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-      >
-        Link 1
-      </a>
-      <a href="#TODO" className="mx-3 font-bold hover:underline">
-        Link 2
-      </a>
- */
