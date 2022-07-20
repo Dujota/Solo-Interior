1e51d79336b6596a47e1b9c262e6e2308c84f0da
dujota <denisdujota@gmail.com>
2022-07-19 20:08:50 -0400

    remove the console.log for deplpoy

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,6 @@ import setupLogRocketReact from 'logrocket-react';
 
 // only initialize when in the browser
 if (typeof window !== 'undefined') {
-  console.log(LogRocket);
   LogRocket.init('solo-interior/solo-interior');
   // plugins should also only be initialized when in the browser
   setupLogRocketReact(LogRocket);
